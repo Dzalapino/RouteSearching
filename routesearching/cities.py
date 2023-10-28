0de93b645bd0e0d4b170fc4af92253276dd70c78
Dzalapino <jpodkomorka19@gmail.com>
2023-10-28 03:41:18 +0200

    Generating cost matrix with different scenarios. Added dicarding connections method

--- routesearching/cities.py
+++ routesearching/cities.py
@@ -1,5 +1,5 @@
 import random
-import math
+import numpy as np
 
 class City:
     def __init__(self):
@@ -13,13 +13,40 @@ def generate_cities(n: int):
         cities.append(City())
     return cities
 
-def count_cost_symmetrical(city1: City, city2: City):
-    return math.sqrt((city2.x - city1.x)**2 + (city2.y - city1.y)**2 + (city2.z - city1.z)**2)
-
-def count_cost_asymmetrical(city1: City, city2: City):
+def count_cost(city1: City, city2: City, symmetrical_problem = True):
     height = city2.z - city1.z
-    if height < 0:
-        height*=11/10
-    else:
-        height*=9/10
-    return math.sqrt((city2.x - city1.x)**2 + (city2.y - city1.y)**2 + (height)**2)
+    if symmetrical_problem == False:
+        if height < 0:
+            height*=11/10
+        else:
+            height*=9/10
+    # Calculate the euclidean distance
+    return np.sqrt((city2.x - city1.x)**2 + (city2.y - city1.y)**2 + (height)**2)
+
+def generate_costs_matrix(cities_list: list[City], symmetrical_problem = True):
+    n = np.size(cities_list)
+    cities_matrix = np.zeros((n, n))
+
+    # Populate the zeros matrix with the travel costs
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                continue
+            cities_matrix[i, j] = count_cost(cities_list[i], cities_list[j], symmetrical_problem)
+    return cities_matrix
+
+def dicard_20percent_connections(cities_matrix):
+    # Generate the lit with unique connections
+    n = np.size(cities_matrix, 0)
+    unique_connections = [(i, j) for i in range(n) for j in range(i + 1, n)]
+    
+    # Shuffle the list and select the first 20% of unique connections to discard
+    random.shuffle(unique_connections)
+    total_to_discard = int(np.floor(0.2 * np.size(unique_connections)))
+    connections_to_discard = unique_connections[:total_to_discard]
+
+    # Discard selected connections
+    for connection in connections_to_discard:
+        i, j = connection
+        cities_matrix[i, j] = 0.
+        cities_matrix[j, i] = 0.
--- routesearching/main.py
+++ routesearching/main.py
@@ -1,4 +1,12 @@
 import cities
 import numpy
 
-cities_list = cities.generate_cities(5)
+cities_list : list[cities.City] = cities.generate_cities(5)
+for city in cities_list:
+    print(city.x, city.y, city.z)
+
+cities_matrix = cities.generate_costs_matrix(cities_list, True)
+print(cities_matrix)
+
+cities.dicard_20percent_connections(cities_matrix)
+print(cities_matrix)
